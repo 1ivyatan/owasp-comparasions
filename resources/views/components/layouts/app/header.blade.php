32206ed5381fafddae41d79b6bcf35f3bbd4929b
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2025-11-30 17:19:37 +0200

    latvianize settings page

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -43,7 +43,7 @@
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
                             </svg>
-                            Settings
+                            {{ __('Iestatījumi'); }}
                         </div>
                     </a>
                     <div class="border-t border-gray-200 dark:border-gray-700"></div>
@@ -57,7 +57,7 @@
                                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                         d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1" />
                                 </svg>
-                                Logout
+                                {{ __('Iziet'); }}
                             </div>
                         </button>
                     </form>
