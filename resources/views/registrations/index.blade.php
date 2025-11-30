ddfccba92b2aa5751ebcd9081a7babc1575ffa21
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2025-11-30 15:12:27 +0200

    prettify header

--- resources/views/registrations/index.blade.php
+++ resources/views/registrations/index.blade.php
@@ -1,17 +1,15 @@
 <x-layouts.app :title="__('Pieteikumi')">
 
-    <div class="mb-6">
-        <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">{{ __('Main')}}</h1>
-        <p class="text-gray-600 dark:text-gray-400 mt-1">{{ __('Welcome to the ') }}</p>
-    </div>
+    <div class="mb-6 flex justify-between">
+        <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">{{ __('Pieteikumi')}}</h1>
 
-    <p> here be crude (yeeeeeees pun intended), now indexed in a controller </p>
+        <x-button tag="a" type="primary" href="{{ route('registrations.create') }}">{{ __('Izveidot') }}</x-button>
+    </div>
 
     @forelse ( $registrations as $registration )
         {{ $registration }}    
     @empty
-        ummm
+        <p class="text-gray-600 dark:text-gray-400 mt-1">{{ __('Nav pieteikumu!') }}</p>
     @endforelse
-    
 
 </x-layouts.app>
