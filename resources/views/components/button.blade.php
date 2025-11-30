8d199ff4be929fc24a49f377b432139a98d7fb8b
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2025-11-30 15:58:36 +0200

    add grey color

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -11,6 +11,7 @@
         match ($type) {
             'primary' => 'bg-blue-600 hover:bg-blue-700 focus:ring-blue-500',
             'danger' => 'bg-red-600 hover:bg-red-700 focus:ring-red-500',
+            'secondary' => 'bg-gray-600 hover:bg-gray-700 focus:ring-gray-500',
         },
     ]);
 @endphp
