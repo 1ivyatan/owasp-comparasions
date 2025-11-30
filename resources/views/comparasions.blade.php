0bf615bb5cf9d234f69b5fc719e38dd7540903fa
Livjatans <143333830+1ivyatan@users.noreply.github.com>
2025-11-30 21:39:10 +0200

    add information about security

--- resources/views/comparasions.blade.php
+++ resources/views/comparasions.blade.php
@@ -10,10 +10,80 @@
     </div>
 
     <div class="mb-6">
-        <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">{{ __('Main')}}</h1>
-        <p class="text-gray-600 dark:text-gray-400 mt-1">{{ __('Welcome to the ') }}</p>
+        <h1 class="text-2xl font-bold text-gray-800 dark:text-gray-100">{{ __('Drošības salīdzinājumi')}}</h1>
     </div>
 
-    <p> something something comparasions </p>
+    <p class="mb-5"> Laravel ietvars jau ir sagatavojis drošības risinājumus salīdzinot ar parasto PHP, kurā pašam izstrādātājam ir jāsagatavo drošības risinājumus. </p>
+
+    <div class="relative flex flex-col h-full text-gray-700 bg-white shadow-md rounded-lg bg-clip-border mb-4">
+    <table class="w-full text-left table-auto text-slate-800">
+        <thead>
+            <tr class="text-slate-500 border-b border-slate-300 bg-slate-50">
+                <th class="p-4">
+                     Problēmas
+                </th>
+                <th class="p-4">PHP</th>
+                <th class="p-4">+ Laravel</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr class="hover:bg-slate-50">
+                <td class="p-4">
+                    <p class="text-sm">
+                        Broken Access Control
+                    </p>
+                </td>
+                <td class="p-4 overflow-scroll ">
+                    <p class="text-sm">
+                        Manuāli izmantot sesijas datus un salīdzināt ar noteiktām vērtībām.
+                    </p>
+                </td>
+                <td class="p-4  overflow-scroll ">
+                    <p class="text-sm">
+                        Laravel piedāvā Middleware komponenti, kura jau filtrē pieprasījumus. Bieži tā tiek izmantota autentifikācijai un CSRF aizsardzībai.
+                    </p>
+                </td>
+            </tr>
+
+            <tr class="hover:bg-slate-50">
+                <td class="p-4">
+                    <p class="text-sm">
+                        Injection
+                    </p>
+                </td>
+                <td class="p-4 overflow-scroll ">
+                    <p class="text-sm">
+                        PHP funkcionalitātes kā PDO piedāvā apstrādāt pašus vaicājumus, lai aizsargātos no injekcijām.
+                        PHP izmanto <pre>htmlspecialchars</pre>, lai filtrētu izvadītos datus.
+                    </p>
+                </td>
+                <td class="p-4  overflow-scroll ">
+                    <p class="text-sm">
+                        Laravel jau sagatavotās komponentes palīdzēs aizsargāties no injekcijas uzbrukumiem. Ievades apstrādē jau piedāvā klases un modeļus, kurā var definēt datu veidus un ierobežojumus, kurā jau apstrādes laikā pēc noteiktiem iestatījumiem pārbauda un filtrē pieprasījumus, kuras iekļauj injekciju aizsardzības. Izvadē ar Blade var izmantot dubultfigūriekavas, kuras jau apstrādā izvadītos datus pret XSS uzbrukumiem.
+                    </p>
+                </td>
+            </tr>
+
+
+            <tr class="hover:bg-slate-50">
+                <td class="p-4">
+                    <p class="text-sm">
+                        CSRF
+                    </p>
+                </td>
+                <td class="p-4 overflow-scroll ">
+                    <p class="text-sm">
+                        Pašam jāveido drošības kodu vai žetonu, kuras gala produkts nav vienmēr drošākā, atkarībā kuras funkcionalitātes tika izmantotas.
+                    </p>
+                </td>
+                <td class="p-4  overflow-scroll ">
+                    <p class="text-sm">
+                        Laravel jau katrā sesijā veido žetonu. Veidlapās ar Blade var ierakstīt <pre>{{ '@'.'csrf' }}</pre>, kas izveidos neredzamu lauku un iesneigšanas laikā mitināta aplikācija salīdzinās ar sesijā izveidotu žetonu un turpinās darbību, ja ir vienādas.
+                    </p>
+                </td>
+            </tr>
+        </tbody>
+    </table>
+    </div>
 
 </x-layouts.app>
